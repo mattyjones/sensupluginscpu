1c2462b9fe20400860937bf6f6de0efc37e72ab0
Matt Jones <mattjones@yieldbot.com>
2016-06-04 22:48:47 -0400

    initial commit (non-working)

--- sensupluginscpu/checkCpuUsage.go
+++ sensupluginscpu/checkCpuUsage.go
@@ -24,6 +24,7 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"time"
 )
 
 // checkCPUUsageCmd represents the checkCpuUsage command
@@ -32,11 +33,27 @@ var checkCPUUsageCmd = &cobra.Command{
 	Short: "Check the total cpu usage",
 	Long:  `Check the total cpu usage on a machine. This will take into account the specific geometry of the given machine.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		geo := AcquireNumCPU()
-		_ = ReadProc()
-		fmt.Println("Number of usable cores", geo)
+
+		val1 := ReadProc()
+		time.Sleep(5)
+		val2 := ReadProc()
+
+
+		fmt.Println(val2[1].User)
+    //total := (val2.User + val2.System + val2.Nice + val2.SoftIRQ + val2.Steal) / (val2.User + val2.System + val2.Nice + val2.SoftIRQ + val2.Idle + val2.IOWait)
+		//fmt.Println("total: ", total)
+		for i := range val1 {
+			cpu := val1[i]
+			fmt.Println("CPU:", cpu)
+		}
+		for i := range val2 {
+			cpu := val2[i]
+			fmt.Println("CPU:", cpu)
+		}
 	},
 }
+
+//CPU_Util = (user+system+nice+softirq+steal)/(user+system+nice+softirq+steal+idle+iowait)
 
 func init() {
 	RootCmd.AddCommand(checkCPUUsageCmd)
